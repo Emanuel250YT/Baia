ff7061306021d4d1ec578fdd6db0fda2b59d02bf
@TheoSempaii <61527540+TheoSempaii@users.noreply.github.com>
2025-03-22 01:00:13 -0300

    get cause

---
+++ app/api/causes/cause/[id]/(route)/route.ts
@@ -0,0 +1,48 @@
+import { APIResponse, APICodes, APIMessages, APIStatus } from "@/classes/APIResponses"
+import CauseModel from "@/database/Cause"
+import { authOptions } from "@/lib/authOptions"
+import connectDatabase from "@/lib/connectDatabase"
+import { sanitizeModel } from "@/lib/sanitize"
+import { getServerSession } from "next-auth"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions)
+
+  if (!session && process.env.ENV != "development") return new APIResponse({
+    body: {},
+    code: APICodes[401],
+    message: APIMessages.UnAuthorized,
+    status: APIStatus.Unauthorized
+  }).response()
+
+  await connectDatabase()
+
+  const id = req.nextUrl.pathname.split('/').pop();
+
+  if (!id || id.length < 1) return new APIResponse({
+    body: {},
+    code: APICodes[404],
+    message: APIMessages.NotFound,
+    status: APIStatus.NotFound
+  }).response()
+
+  const dbCause = await CauseModel.findOne({
+    uuid: id
+  })
+
+  if (!dbCause) return new APIResponse({
+    body: {},
+    code: APICodes[404],
+    message: APIMessages.NotFound,
+    status: APIStatus.NotFound
+  }).response()
+
+
+  return new APIResponse({
+    body: sanitizeModel(dbCause),
+    code: APICodes[200],
+    message: APIMessages.OK,
+    status: APIStatus.OK
+  }).response()
+}
